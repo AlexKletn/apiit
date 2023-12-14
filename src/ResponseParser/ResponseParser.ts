859f377c98dffd740883965b45b4f7f2a51713a2
Alexandr Kletnyy <q@obscur.space>
2023-12-14 22:23:42 +0300

    fix: request error

--- src/ResponseParser/ResponseParser.ts
+++ src/ResponseParser/ResponseParser.ts
@@ -1,13 +1,13 @@
-import type { Response, ResponseOptions } from './types';
+import type { ResponseOptions, ResponseSuccessful } from './types';
 
 class ResponseParser {
   static parse<ResponseType>(
     { data, headers }: ResponseOptions<ResponseType>,
-  ): Response<ResponseType> {
+  ): ResponseSuccessful<ResponseType> {
     const contentDisposition = headers ? headers['content-disposition'] : undefined;
 
-    const contentType = headers ? headers['content-type'] : undefined;
-    const fileName = contentDisposition
+    const contentType: string = headers ? headers['content-type'] as string : undefined;
+    const fileName = (contentDisposition as string)
       ?.replace(/[A-Za-z\d; ]*?filename="?([\w\-+\d(){}.']+)"?/i, '$1');
 
     return {
--- src/ResponseParser/types.ts
+++ src/ResponseParser/types.ts
@@ -1,15 +1,15 @@
 export interface ResponseOptions<ResponseType> {
   data: ResponseType
   headers: {
-    [key: string]: unknown
+    [key: string]: string | number
   }
 }
 
-interface ResponseFailed {
+export interface ResponseFailed {
   error?: Error
 }
 
-interface ResponseSuccessful<ResponseType> extends ResponseOptions<ResponseType> {
+export interface ResponseSuccessful<ResponseType> extends ResponseOptions<ResponseType> {
   contentType?: string,
   fileName?: string,
 }
